76fe0a8a187aa5af9883cf91ef2018dc95c3ab52
niu.filippov <niu.filippov@seversal.com>
2022-09-21 22:47:08 +0300

    New commit

---
+++ seleniumAutotests/src/main/java/data/TestData.java
@@ -0,0 +1,6 @@
+package data;
+
+public class TestData {
+
+    public static final String URL = "https://django-helpdesk-demo.herokuapp.com";
+}
---
+++ seleniumAutotests/src/main/java/helpDesk/HelpDeskTest.java
@@ -0,0 +1,28 @@
+package helpDesk;
+
+import core.BaseSeleniumTest;
+import data.TestValues;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import readProperties.ConfigProvider;
+
+import static helpers.StringModifier.getUniqueString;
+
+public class HelpDeskTest extends BaseSeleniumTest {
+
+    public static void checkTicket(){
+        String title = getUniqueString(TestValues.TEST_TITLE);
+        TicketPage ticketPage = new MainPage().createTicket(title, TestValues.TEST_BODY, TestValues.TEST_EMAIL)
+                .openLoginPage()
+                .auth(ConfigProvider.DEMO_LOGIN, ConfigProvider.DEMO_PASSWORD)
+                .findTicket(title);
+        Assertions.assertTrue(ticketPage.getTitle().contains(title));
+        Assertions.assertEquals(ticketPage.getBody(), TestValues.TEST_BODY);
+        Assertions.assertEquals(ticketPage.getEmail(), TestValues.TEST_EMAIL);
+    }
+
+    @Test
+    public void runCheckTicket() {
+        checkTicket();
+    }
+}
